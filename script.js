c6f546d9d80a41dff46f69a88bfa57a79cba1b33
Megna Shrestha <megna.sth1@gmail.com>
2024-06-03 12:31:57 +0545

    all basic js complete

--- script.js
+++ script.js
@@ -56,7 +56,43 @@ aboutmePage.addEventListener('mouseenter', function() {
     document.getElementById('work1').style.backgroundColor = '';
     document.getElementById('oicons').style.color = '#6CBBCC'; 
   });
+ 
+    // --------------------------------------mouseenterontestimonialPage---------------------------------------------------
 
+    document.getElementById('testimonial').addEventListener('mouseenter', function() {
+      document.getElementById('resume1').style.backgroundColor = '#6CBBCC';
+      document.getElementById('ricons').style.color = 'white'; 
+  
+    })
+    document.getElementById('testimonial').addEventListener('mouseleave', function() {
+      document.getElementById('resume1').style.backgroundColor = '';
+      document.getElementById('ricons').style.color = '#6CBBCC'; 
+    });
+   
+ // --------------------------------------mouseenteronblogPage---------------------------------------------------
+
+        document.getElementById('blogpage').addEventListener('mouseenter', function() {
+          document.getElementById('blog1').style.backgroundColor = '#6CBBCC';
+          document.getElementById('bicons').style.color = 'white'; 
+      
+        })
+        document.getElementById('blogpage').addEventListener('mouseleave', function() {
+          document.getElementById('blog1').style.backgroundColor = '';
+          document.getElementById('bicons').style.color = '#6CBBCC'; 
+        });
+
+ // --------------------------------------mouseenteronContactMePage---------------------------------------------------
+
+ document.getElementById('contactmepage').addEventListener('mouseenter', function() {
+  document.getElementById('contact1').style.backgroundColor = '#6CBBCC';
+  document.getElementById('cicons').style.color = 'white'; 
+
+})
+document.getElementById('contactmepage').addEventListener('mouseleave', function() {
+  document.getElementById('contact1').style.backgroundColor = '';
+  document.getElementById('cicons').style.color = '#6CBBCC'; 
+});        
+       
 // --------------------------------------------------Onclickfunctions----------------------------------------------
 document.getElementById('aboutme1').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent default link behavior
@@ -67,4 +103,52 @@ document.getElementById('work1').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent default link behavior
     document.getElementById('workpage').scrollIntoView({ behavior: 'smooth' }); // Scroll to the about section
   });
+
+  document.getElementById('resume1').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    document.getElementById('testimonial').scrollIntoView({ behavior: 'smooth' }); // Scroll to the about section
+  });
+
+  document.getElementById('blog1').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    document.getElementById('blogpage').scrollIntoView({ behavior: 'smooth' }); // Scroll to the about section
+  });
+
+  document.getElementById('contact1').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    document.getElementById('contactmepage').scrollIntoView({ behavior: 'smooth' }); // Scroll to the about section
+  });
   
+  document.getElementById('button1').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    document.getElementById('contactmepage').scrollIntoView({ behavior: 'smooth' }); // Scroll to the about section
+  });
+  
+  // ----------------------------------------------carousel---------------------------------------------------
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
